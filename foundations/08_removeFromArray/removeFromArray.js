93e0960f49f07f33c49794ccfe7136a99a5723bc
mishmishel <misheltyurkova@gmail.com>
2026-01-18 14:40:07 +1100

    Remove from Array exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    const args = Array.from(arguments);
+    args.splice(0, 1);
+    result = arr.filter((item) => !args.includes(item));
+    return result
 };
 
 // Do not edit below this line
