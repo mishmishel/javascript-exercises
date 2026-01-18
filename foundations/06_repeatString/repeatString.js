3f1b65d083a31d957d627d657e62bcc7991c37ad
mishmishel <misheltyurkova@gmail.com>
2026-01-18 14:25:27 +1100

    Repeat String exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return 'ERROR'
+    }
 
+    let current = ''
+    for (let i =0; i < num; i++) {
+        current += str
+    }
+    return current
 };
 
 // Do not edit below this line
