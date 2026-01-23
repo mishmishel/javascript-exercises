f24235ca8a228b0c110e234ee3f97d0bb9d3a2c7
mishmishel <misheltyurkova@gmail.com>
2026-01-23 23:15:04 +1100

    Find the oldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const oldest = arr.sort((a, b) => {
+        if (!a.yearOfDeath) {
+            a.yearOfDeath = (new Date()).getFullYear(); // Still alive, get current year
+        } else if (!b.yearOfDeath) {
+            b.yearOfDeath = (new Date()).getFullYear(); // Still alive get current year
+        }
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        return lastGuy > nextGuy ? -1 : 1
+    })
+    return oldest[0];
 };
 
 // Do not edit below this line
