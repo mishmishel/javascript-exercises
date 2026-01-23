68b175f8718106596203a3a2126d023494ae3b93
mishmishel <misheltyurkova@gmail.com>
2026-01-23 23:04:40 +1100

    Function getTheTitles

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    return arr.map((object => {
+        return object.title;
+    }))
 };
 
 // Do not edit below this line
