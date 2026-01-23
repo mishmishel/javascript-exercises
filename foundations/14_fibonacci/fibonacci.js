6b0bc9a3940859f6deb3845fde9a1f6099fabd91
mishmishel <misheltyurkova@gmail.com>
2026-01-23 23:03:02 +1100

    Fibonacci Sequence Function

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) { 
+    if (num == 0) {
+        return 0;
+    } else if (num < 0) {
+        return 'OOPS';
+    }
 
+    prev1 = 1
+    prev2 = 1
+    current = 1
+    for (let i = 2; i < num; i++) {
+       current = prev1 + prev2
+       prev2 = current
+       prev1 = current - prev1 
+    }
+    return current
 };
 
 // Do not edit below this line
