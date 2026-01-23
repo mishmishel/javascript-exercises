1c61214d9a1cd8c747f9a2fe704b1e041a984328
mishmishel <misheltyurkova@gmail.com>
2026-01-23 19:34:42 +1100

    Calculator Exercise with arithmetic functions

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, num) => {
+    return total += num
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, num) => {
+    return total *= num;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0) {
+    return 1;
+  }
+
+  let total = num;
+	for (let i = num - 1; i > 0; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
