112c43c5f5350e27c87a8b1834ef2d398f7b89a8
mishmishel <misheltyurkova@gmail.com>
2026-01-23 21:38:18 +1100

    Palindrome Javascript Exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    lower = str.toLowerCase();
+    noPunctuation = lower.replace(/[,!.]/g, "");
+    formatted = noPunctuation.replaceAll(" ", "");
+    let i = 0
+    let j = formatted.length - 1
+    while (i < j) {
+        console.log(i + ' ' + formatted[i]);
+        console.log(j + ' ' + formatted[j]);
+        if (formatted[i] != formatted[j]) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
